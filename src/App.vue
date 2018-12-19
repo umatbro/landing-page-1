36721f55d18c3608f1370a58755999e54e2423cb
umatbro <umatbroo@gmail.com>
2018-12-20 00:46:42 +0100

    remove commented out code

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,4 @@
 <template>
-  <!-- <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div> -->
   <v-app dark>
     <v-toolbar>
         <router-link to="/"><v-btn>Home</v-btn></router-link>
@@ -23,7 +16,6 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  // color: #2c3e50;
 }
 #nav {
   padding: 30px;
