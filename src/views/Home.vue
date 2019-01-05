ac72e970211be33e7db65c350d60e09c1cda7f76
umatbro <umatbroo@gmail.com>
2019-01-05 12:14:16 +0100

    remove unused code
    remove HelloWorld example generated by vue-cli

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,6 @@
 <script lang="ts">
 import Vue from 'vue';
 import HomeContent from '@/components/HomeContent.vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default Vue.extend({
   name: 'home',
